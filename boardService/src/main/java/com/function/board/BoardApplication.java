139109f8a6158710f6e913eb0d99cca2be04d584
mmy789 <mmy789@naver.com>
2023-03-07 12:30:16 +0900

    Feat: Board entity 생성

--- boardService/src/main/java/com/function/board/BoardApplication.java
+++ boardService/src/main/java/com/function/board/BoardApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.function.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo2Application {
+public class BoardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo2Application.class, args);
+		SpringApplication.run(BoardApplication.class, args);
 	}
 
 }
---
+++ boardService/src/main/java/com/function/board/domain/board/Board.java
@@ -0,0 +1,56 @@
+package com.function.board.domain.board;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import com.function.board.domain.BaseTimeEntity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "board")
+@SQLDelete(sql = "UPDATE board SET is_deleted = true WHERE board_id=?")
+@Where(clause = "is_deleted = false")
+public class Board extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String content;
+
+	@Column(nullable = false)
+	private String writer;
+
+	@Column(name = "like_count")
+	private int likeCount;
+
+	@Column(name = "dislike_count")
+	private int dislikeCount;
+
+	@Builder
+	public Board(String content, String writer) {
+		this.content = content;
+		this.writer = writer;
+		this.likeCount = 0;
+		this.dislikeCount = 0;
+	}
+
+	public void update(String content) {
+		this.content = content;
+	}
+
+}
