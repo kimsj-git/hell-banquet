6d54d81cb082b68fcd1a78e8ec7f1a0695bcfd47
강원경 <kokk012@naver.com>
2023-03-08 09:15:29 +0900

    Style: 메소드 이름에 포함된 도메인 수정

--- src/main/java/com/hellsfood/api/users/controller/UserController.java
+++ src/main/java/com/hellsfood/api/users/controller/UserController.java
@@ -38,12 +38,12 @@ public class UserController {
 
 	@PutMapping("/info/{id}")
 	@ApiOperation(value = "정보 변경", notes = "갱신된 사용자 정보를 {id}를 PK로 가지는 레코드에 적용한다.")
-	public ResponseEntity updateMember(
+	public ResponseEntity updateUser(
 		@PathVariable @ApiParam(value = "회원정보를 수정할 사용자의 {id}", required = true) String id,
 		@RequestBody @ApiParam(value = "수정할 내용이 담긴 데이터 객체", required = true) UpdateRequestDto requestDto,
 		HttpServletRequest request) {
 		String accessToken = request.getHeader("Authorization").substring(7);
-		String updatedUserId = userService.updateInfo(id, requestDto, accessToken);
+		String updatedUserId = userService.updateUser(id, requestDto, accessToken);
 		if (updatedUserId == null) {
 			return ResponseEntity.status(HttpStatus.FORBIDDEN).body(id + " 사용자의 수정 권한이 없는 사용자입니다.");
 		}
@@ -52,8 +52,8 @@ public class UserController {
 
 	@GetMapping("/info/{id}")
 	@ApiOperation(value = "정보 조회", notes = "{id}에 해당하는 사용자 정보를 DB에서 가져온다.")
-	public ResponseEntity findById(@PathVariable @ApiParam(value = "회원정보를 조회할 사용자의 {id}", required = true) String id) {
-		User user = userService.findByUserId(id);
+	public ResponseEntity getUser(@PathVariable @ApiParam(value = "회원정보를 조회할 사용자의 {id}", required = true) String id) {
+		User user = userService.getUser(id);
 		if (user.getDelFlag() != null) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("탈퇴한 사용자입니다.");
 		} else {
@@ -63,7 +63,7 @@ public class UserController {
 
 	@PostMapping("/delete/{id}")
 	@ApiOperation(value = "회원 탈퇴", notes = "{id}의 사용자 정보에 탈퇴일(del_flag)을 기록한다.")
-	public ResponseEntity deleteMember(@PathVariable @ApiParam(value = "탈퇴할 회원 ID", required = true) String id,
+	public ResponseEntity deleteUser(@PathVariable @ApiParam(value = "탈퇴할 회원 ID", required = true) String id,
 		HttpServletRequest request) {
 		String accessToken = request.getHeader("Authorization").substring(7);
 		String changedId = userService.deleteUser(id, accessToken);
