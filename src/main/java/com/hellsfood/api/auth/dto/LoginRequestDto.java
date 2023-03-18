2c5b11279f3d20cb2f516f42c32e35c538fa654b
강원경 <kokk012@naver.com>
2023-03-18 17:20:46 +0900

    Refactor: 불필요한 설명 삭제
    Swagger에서 해당 설명이 노출되지 않음.

--- src/main/java/com/hellsfood/api/auth/dto/LoginRequestDto.java
+++ src/main/java/com/hellsfood/api/auth/dto/LoginRequestDto.java
@@ -1,7 +1,6 @@
 package com.hellsfood.api.auth.dto;
 
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +10,8 @@ import lombok.NoArgsConstructor;
 @Getter
 @ApiModel("사용자 로그인 요청 정보")
 public class LoginRequestDto {
-	@ApiModelProperty(name = "입력한 ID")
 	private String userId;
 
-	@ApiModelProperty(name="입력한 비밀번호")
 	private String password;
 
 }
