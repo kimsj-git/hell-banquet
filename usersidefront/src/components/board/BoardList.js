002f91c1bf75bd37c29ec6485211439181b2968d
sjsj.lim <sjjj.lim@gmail.com>
2023-03-20 22:36:16 +0900

    Feat: 무한 스크롤 구현

--- usersidefront/src/components/board/BoardList.js
+++ usersidefront/src/components/board/BoardList.js
@@ -1,11 +1,13 @@
 import angryJenkins from "../../assets/images/angryJenkins.png"
 import BoardListItem from "./BoardListItem"
 
+import { useRef, useEffect, useState } from "react"
+
 function BoardList() {
 
     const lorem = "Lorem ipsuetur adipiscing elit. Vestibulum porta odio eros, eget dignissim felis egestas vitae. Mauris sit amet est nec eros accumsan eleifend. Etia".slice(0, 100)
 
-    const articles = [
+    const [articles, setArticles] = useState([
         {content: lorem, src: angryJenkins},
         {content: lorem, src: angryJenkins},
         {content: lorem, src: angryJenkins},
@@ -15,14 +17,40 @@ function BoardList() {
         {content: lorem, src: angryJenkins},
         {content: lorem, src: angryJenkins},
         {content: lorem, src: angryJenkins},
-    ]
+    ])
+    
+    const articleListRef = useRef(null);
+    useEffect(() => {
+        const observerOptions = {
+            root: null,
+            rootMargin: '100px',
+            threshold: 0.5
+        };
+        
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    // article이 2배씩 증식함
+                    console.log('Article is now visible!', entry.target);
+                    setArticles(articles.flatMap(article => [article, article]))
+                }
+            });
+        }, observerOptions);
+        
+        observer.observe(articleListRef.current.lastChild);
+
+        return () => observer.disconnect();
+    }, [articles]);
 
     return (
-            articles.map((article, index) => {
+        <div ref={articleListRef} >
+            {articles.map((article, index) => {
                 return (
                     <BoardListItem article={article} index={index} key={index} />
                 )
-            })
+            })}
+        </div>
+
     )
 }
 
