0395658318e0302e57f17955514eab2acceb1973
강원경 <kokk012@naver.com>
2023-03-21 13:05:38 +0900

    Fix: RefreshToken의 userId 속성 수정
    Transactional 선언으로 관련 문제
    - 기존에 비정상 로그인된 토큰이 삭제되지 않은 경우 삭제 -> 재발급 필요
    - 이 과정에서 Transactional로 인해 삭제되었다 하더라도 commit 전까지 기존 정보가 남은 것으로 인식됨
    - unique 속성 삭제

--- src/main/java/com/hellsfood/api/tokens/data/RefreshToken.java
+++ src/main/java/com/hellsfood/api/tokens/data/RefreshToken.java
@@ -30,6 +30,6 @@ public class RefreshToken {
 	@Column(nullable = false)
 	private String refreshToken;
 
-	@Column(nullable = false, unique=true)
+	@Column(nullable = false)
 	private String userId;
 }
