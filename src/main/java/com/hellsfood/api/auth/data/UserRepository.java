ddf7a6af93e3419f295df4d26e7f517c5efee143
강원경 <kokk012@naver.com>
2023-03-21 17:14:50 +0900

    Style: 불필요한 JPA 함수 선언 삭제

--- src/main/java/com/hellsfood/api/auth/data/UserRepository.java
+++ src/main/java/com/hellsfood/api/auth/data/UserRepository.java
@@ -1,6 +1,5 @@
 package com.hellsfood.api.auth.data;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,8 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByUserId(String userId);
-
-	Optional<List<Role>> findRolesByUserId(String userId);
 
 	@Query("SELECT u.userId from User u where u.email=:email")
 	Optional<String> findUserIdByEmail(String email);
