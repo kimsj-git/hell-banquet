fa50544c7f50660af12b8f0c09c0cfa13d4047eb
강원경 <kokk012@naver.com>
2023-03-24 13:22:22 +0900

    Style: 불필요한 코드 삭제

--- src/main/java/com/hellsfood/apigateway/users/data/UserRepository.java
+++ src/main/java/com/hellsfood/apigateway/users/data/UserRepository.java
@@ -1,19 +1,9 @@
 package com.hellsfood.apigateway.users.data;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByUserId(String userId);
-
-	List<Role> getRolesByUserId(String userId);
-
-	@Query("SELECT u.userId from User u where u.email=:email and u.delFlag is null")
-	Optional<String> findActiveUserIdByEmail(String email);
-
-	@Query("SELECT MAX(u.id) from User u")
-	Long getLastUID();
 }
