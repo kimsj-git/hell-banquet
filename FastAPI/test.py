6fb1b78e5df41edc4ce830a5da87ff13e3081940
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2023-04-03 00:02:43 +0900

    Feat: 음식양 인식 모델 추가
    Former-commit-id: 2dad2e8a76d85986d95d256c5f59a0c49220802e

--- FastAPI/test.py
+++ FastAPI/test.py
@@ -10,6 +10,12 @@ import uuid
 
 import drawjanbani
 
+import sys
+import torch
+sys.path.insert(0,'./yolov7/seg')
+from yolov7.seg.segment import predict_test
+device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+
 app = FastAPI()
 
 load_dotenv()
@@ -39,25 +45,28 @@ async def draw_is_correct(image: UploadFile = File(), category: str = Form()):
 
 
 @app.post("/ai/janban/")
-async def check_janban(image: UploadFile = File()):
+# async def check_janban(image: UploadFile = File()):
+async def check_janban():
 
     # 아래에 코드 작성
+    ratio_with_plate, img = predict_test.run(weights='best.pt', device=device, conf_thres=0.5, imgsz=(480,640), source='test_images/food', nosave=False)
+    return JSONResponse(content={"status": "true", "ratio": ratio_with_plate.item()})
+    # food_check = detect.detect()
 
 
+    # file_name = str(uuid.uuid4()) + ".png"
+    # contents = await image.read() # image를 s3에 넣을 수 있는 형태로
 
-    file_name = str(uuid.uuid4()) + ".png"
-    contents = await image.read() # image를 s3에 넣을 수 있는 형태로
+    # try:
+    #     client_s3.put_object(
+    #         Body=contents,
+    #         Bucket=BUCKET,
+    #         Key=file_name,
+    #         ContentType='image/png'
+    #     )
+    #     s3_file_path = f'https://leftover-bucket.s3.ap-northeast-2.amazonaws.com/{file_name}'
 
-    try:
-        client_s3.put_object(
-            Body=contents,
-            Bucket=BUCKET,
-            Key=file_name,
-            ContentType='image/png'
-        )
-        s3_file_path = f'https://leftover-bucket.s3.ap-northeast-2.amazonaws.com/{file_name}'
+    #     return JSONResponse(content={"s3_file_path": s3_file_path})
 
-        return JSONResponse(content={"s3_file_path": s3_file_path})
-
-    except:
-        return JSONResponse(content={"s3_file_path": "failed"})
+    # except:
+    #     return JSONResponse(content={"s3_file_path": "failed"})
