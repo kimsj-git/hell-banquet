1da0b083929c4874b234672c4173fc32fcde5d0e
mmy789 <mmy789@naver.com>
2023-04-03 09:19:22 +0900

    Refactor: CommentController NamingConvention 통일

--- boardService/src/main/java/com/hellsfood/api/CommentController.java
+++ boardService/src/main/java/com/hellsfood/api/CommentController.java
@@ -29,26 +29,26 @@ public class CommentController {
 
 	@ApiOperation(value = "댓글 생성")
 	@PostMapping()
-	public ResponseEntity<Long> save(@RequestBody CommentSaveRequestDto requestDto) {
+	public ResponseEntity<Long> createComment(@RequestBody CommentSaveRequestDto requestDto) {
 		commentService.save(requestDto);
 		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 
-	@ApiOperation(value = "{id}에 해당하는 댓글 단건 조회")
+	@ApiOperation(value = "댓글 상세 조회 - find by id")
 	@GetMapping("/{id}")
-	public ResponseEntity<CommentListResponseDto> findById(@PathVariable("id") Long id) {
+	public ResponseEntity<CommentListResponseDto> getOneCommentById(@PathVariable("id") Long id) {
 		return ResponseEntity.ok(commentService.findById(id));
 	}
 
-	@ApiOperation(value = "{id}에 해당하는 댓글 수정")
+	@ApiOperation(value = "댓글 수정")
 	@PutMapping("/{id}")
-	public ResponseEntity<Long> update(@PathVariable Long id, @RequestBody CommentUpdateRequestDto requestDto) {
+	public ResponseEntity<Long> updateComment(@PathVariable Long id, @RequestBody CommentUpdateRequestDto requestDto) {
 		return ResponseEntity.ok(commentService.update(id, requestDto));
 	}
 
-	@ApiOperation(value = "{id}에 해당하는 댓글 삭제")
+	@ApiOperation(value = "댓글 삭제")
 	@DeleteMapping("/{id}")
-	public void delete(@PathVariable Long id) {
+	public void deleteComment(@PathVariable Long id) {
 		commentService.delete(id);
 	}
 
