d92396fd37790037ca0741dfd65361666d4a3106
yadon <mucho7@gmail.com>
2023-04-03 12:23:19 +0900

    Feat: 러프한 Daily Rank

---
+++ usersidefront/src/components/analytics/OverviewRanking.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import { getDailyRank } from "../../api/leftover";
+
+function OverviewRanking() {
+  const [result, setResult] = useState();
+
+  useEffect(() => {
+    const handleGetDailyRank = async () => {
+      await getDailyRank(
+        { userId: "ssafy" },
+        (data) => {
+          console.log(data);
+          return data.data;
+        },
+        (err) => console.log(err)
+      ).then((data) => setResult(data));
+    };
+    handleGetDailyRank();
+  }, []);
+  return <div>{result}</div>;
+}
+
+export default OverviewRanking;
--- usersidefront/src/pages/LandingPage.js
+++ usersidefront/src/pages/LandingPage.js
@@ -1,46 +1,43 @@
-import { RecommendAritcle } from "../components/board"
-import { LogedPageTemplate, } from "../components/common"
-import { OverviewDailyJanban } from "../components/janban"
+import { RecommendAritcle } from "../components/board";
+import { LogedPageTemplate } from "../components/common";
+import { OverviewDailyJanban } from "../components/janban";
 
-import styled from "styled-components"
-import {  Container } from "@mui/material"
+import styled from "styled-components";
+import { Container } from "@mui/material";
+import OverviewRanking from "../components/analytics/OverviewRanking";
 
 function LandingPage() {
-    const hour = new Date().getHours()
-   
-    return(
-        <>
-        <LogedPageTemplate />
-        <Container style={styleForContainer}>
-            <JanvanSection>
-                <OverviewDailyJanban />
-            </JanvanSection>
-            {hour < 14 ? 
-            <DailyMenuSection>
-                <div>
-                    아직 14시가 안지났나?
-                </div>
-            </DailyMenuSection>
-            :
-            <DailyRankSection>
-                 <div>
-                    이제 랭킹이 보일 때야!
-                </div>
-            </DailyRankSection>
-            }
-            <RecommendArticleSection>
-                <RecommendAritcle />
-            </RecommendArticleSection>
-        </Container>
-        </>
-    )
+  const hour = new Date().getHours();
+
+  return (
+    <>
+      <LogedPageTemplate />
+      <Container style={styleForContainer}>
+        <JanvanSection>
+          <OverviewDailyJanban />
+        </JanvanSection>
+        {hour < 14 ? (
+          <DailyMenuSection>
+            <div>아직 14시가 안지났나?</div>
+          </DailyMenuSection>
+        ) : (
+          <DailyRankSection>
+            <OverviewRanking />
+          </DailyRankSection>
+        )}
+        <RecommendArticleSection>
+          <RecommendAritcle />
+        </RecommendArticleSection>
+      </Container>
+    </>
+  );
 }
 const styleForContainer = {
-    display: 'flex', 
-    flexDirection: 'column', 
-    justifyContent: 'center', 
-    alignItems:'center',
-}
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+};
 
 const styleForSection = `
     width: 100%;
@@ -51,28 +48,27 @@ const styleForSection = `
     border-radius: 30px;
 
     display: flex;
-`
+`;
 
 const JanvanSection = styled.section`
-    ${styleForSection}
-    height: 270px;
-
-`
+  ${styleForSection}
+  height: 270px;
+`;
 const DailyMenuSection = styled.section`
-    ${styleForSection}
-    height: 280px;
-`
+  ${styleForSection}
+  height: 280px;
+`;
 
 const DailyRankSection = styled.section`
-    ${styleForSection}
-    height: 280px;
-`
+  ${styleForSection}
+  height: 280px;
+`;
 
 const RecommendArticleSection = styled.section`
-    ${styleForSection}
-    height: 260px;
-    justify-content: center;
-    margin-bottom: 110px;
-`
+  ${styleForSection}
+  height: 260px;
+  justify-content: center;
+  margin-bottom: 110px;
+`;
 
-export default LandingPage
+export default LandingPage;
