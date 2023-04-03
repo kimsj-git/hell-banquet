324461ef5e23963f951c089fd944fe372efc7da4
Jaegyeong Jo <89237865+Joripje@users.noreply.github.com>
2023-04-03 13:16:32 +0900

    Feat: ABCGame 카메라 추가
    Former-commit-id: df8b32082c16f41512ffc21f628cb5947964ca41

--- cookiegame_js/cookie_phaser/src/component/cookiegame/RunGame.jsx
+++ cookiegame_js/cookie_phaser/src/component/cookiegame/RunGame.jsx
@@ -5,7 +5,7 @@
 // };
 
 // export default RunGame;
-
+import ABCam from "./ABCam";
 import Phaser from "phaser";
 import { Scene1 } from "./Scene1";
 import { Scene2 } from "./Scene2";
@@ -16,7 +16,7 @@ import { useEffect, useRef } from "react";
 import styled from "styled-components";
 
 const RunGame = () => {
-  const videoRef = useRef(null);
+  // const videoRef = useRef(null);
 
   useEffect(() => {
     const config = {
@@ -33,21 +33,22 @@ const RunGame = () => {
 
     new Phaser.Game(config);
 
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: false })
-      .then((stream) => {
-        // 스트림을 비디오에 할당
-        videoRef.current.srcObject = stream;
-      })
-      .catch((error) => {
-        console.error("Error accessing media devices.", error);
-      });
+    // navigator.mediaDevices
+    //   .getUserMedia({ video: true, audio: false })
+    //   .then((stream) => {
+    //     // 스트림을 비디오에 할당
+    //     videoRef.current.srcObject = stream;
+    //   })
+    //   .catch((error) => {
+    //     console.error("Error accessing media devices.", error);
+    //   });
   }, []);
 
   return (
     <Container>
       <WebCamContainer>
-        <Video ref={videoRef} autoPlay playsInline muted />
+        {/* <Video ref={videoRef} autoPlay playsInline muted /> */}
+        <ABCam />
       </WebCamContainer>
       <PhaserContainer id="phaser-container" />
     </Container>
@@ -72,10 +73,10 @@ const PhaserContainer = styled.div`
   height: 50%;
 `;
 
-const Video = styled.video`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
+// const Video = styled.video`
+//   width: 100%;
+//   height: 100%;
+//   object-fit: cover;
+// `;
 
 export default RunGame;
