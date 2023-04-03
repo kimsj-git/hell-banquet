23b09e3a82af4c49a38c389ab5187dafaea51488
mmy789 <mmy789@naver.com>
2023-04-03 15:24:15 +0900

    Docs: Swagger 설명 추가

--- boardService/src/main/java/com/hellsfood/api/RatingController.java
+++ boardService/src/main/java/com/hellsfood/api/RatingController.java
@@ -12,6 +12,7 @@ import com.hellsfood.domain.rating.Rating;
 import com.hellsfood.dto.RatingRequestDto;
 import com.hellsfood.service.RatingService;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
 // @CrossOrigin(origins = "http://localhost:3000")
@@ -22,6 +23,7 @@ public class RatingController {
 
 	private final RatingService ratingService;
 
+	@ApiOperation(value = "게시글 좋아요")
 	@PutMapping("/like")
 	public ResponseEntity<Rating> likeToBoard(@PathVariable Long boardId,
 		@RequestBody RatingRequestDto dto) {
@@ -29,6 +31,7 @@ public class RatingController {
 		return ResponseEntity.ok(rating);
 	}
 
+	@ApiOperation(value = "게시글 싫어요")
 	@PutMapping("/dislike")
 	public ResponseEntity<Rating> dislikeToBoard(@PathVariable Long boardId,
 		@RequestBody RatingRequestDto dto) {
@@ -36,12 +39,14 @@ public class RatingController {
 		return ResponseEntity.ok(rating);
 	}
 
+	@ApiOperation(value = "특정 게시글 좋아요 개수 조회")
 	@GetMapping("/like/count")
 	public ResponseEntity<Integer> getLikeCount(@PathVariable Long boardId) {
 		Rating rating = ratingService.getRating(boardId);
 		return ResponseEntity.ok(rating.getLikeCount());
 	}
 
+	@ApiOperation(value = "특정 게시글 싫어요 개수 조회")
 	@GetMapping("/dislike/count")
 	public ResponseEntity<Integer> getDislikeCount(@PathVariable Long boardId) {
 		Rating rating = ratingService.getRating(boardId);
--- boardService/src/main/java/com/hellsfood/dto/RatingRequestDto.java
+++ boardService/src/main/java/com/hellsfood/dto/RatingRequestDto.java
@@ -6,5 +6,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class RatingRequestDto {
+
 	String userId;
+
 }
