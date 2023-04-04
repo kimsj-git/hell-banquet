b5c135f3611fa7063dc50bb7c879e73a96c01c75
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2023-04-04 23:24:26 +0900

    Feat: 반응형 디자인 추가

--- usersidefront/src/App.js
+++ usersidefront/src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import { RouterProvider } from 'react-router-dom';
-import router from './pages';
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./pages";
+import Grid from "@mui/system/Unstable_Grid/Grid";
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <Grid container>
+      <Grid item sm={3} md={4}></Grid>
+      <Grid item xs={12} sm={6} md={4} style={{ backgroundColor: "#EDEBE9" }}>
+        <RouterProvider router={router} />
+      </Grid>
+      <Grid item sm={3} md={4}></Grid>
+    </Grid>
   );
 }
 
