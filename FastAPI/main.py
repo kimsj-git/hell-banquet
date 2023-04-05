d4efcf4f9d611666e2bcb97a12dac20971db6e2d
임소정 <sjjj.lim@gmail.com>
2023-04-05 20:14:17 +0900

    Update: mixed content 해결, CORS 추가 Former-commit-id: 13ccc644380ef9c5d9bafef4c86e6adb15190742

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, File, Form, UploadFile
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
 import drawjanbani
 import foodseg
 
 app = FastAPI()
-# app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 # SSL 설정을 적용하기 위한 미들웨어 추가
 app.add_middleware(HTTPSRedirectMiddleware)
