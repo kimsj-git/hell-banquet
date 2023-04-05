7cfec349e7b8a02cccc9b4b26a9bce77d69e37cd
Jaegyeong Jo <89237865+Joripje@users.noreply.github.com>
2023-04-05 15:04:00 +0900

    Feat: 잔반이 통신코드 작성

--- usersidefront/src/components/draw/DrawSubject.js
+++ usersidefront/src/components/draw/DrawSubject.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 function DrawSubject(params) {
   const { subjectIndex } = params;
+  // const subject = '조재경'
   const subjects = [
     "나비넥타이",
     "케이크",
@@ -19,12 +20,10 @@ function DrawSubject(params) {
     "트럼펫",
     "우산",
   ];
-  // const subject = '조재경'
-
   return (
     <ColumnBox>
       <MyTypo fontSize="40">주제</MyTypo>
-      <MyTypo fontSize="100">{subjects[subjectIndex]}</MyTypo>
+      {subjectIndex && <MyTypo fontSize="100">{subjects[subjectIndex]}</MyTypo>}
     </ColumnBox>
   );
 }
