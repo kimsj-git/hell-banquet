6683e3c3c25e0c02171b1d8ef26077ed265fda27
yadon <mucho7@gmail.com>
2023-04-15 03:47:19 +0900

    Feat: 랜덤문제 출력

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -14,19 +14,69 @@ interface DestinationProps {
 }
 
 const GameBoard: React.FC = () => {
-  const problem: Problem = {
+  const initialProblem: Problem = {
     id: 1,
     items: [
-      [-1, 1, -1, 3, 2, -1],
-      [0, 0, 0, 0, 0, 1],
-      [0, 0, 0, 0, 0, -1],
-      [0, 0, 0, 0, 0, 2],
-      [0, 0, 0, 0, 0, -1],
-      [0, 0, 0, 0, 0, 3],
+      [-1, 1, -1, 3, 2, -1, -1],
+      [-1, 0, 0, 0, 0, 0, 1],
+      [-1, 0, 0, 0, 0, 0, -1],
+      [-1, 0, 0, 0, 0, 0, 2],
+      [-1, 0, 0, 0, 0, 0, -1],
+      [-1, 0, 0, 0, 0, 0, 3],
+      [-1, -1, -1, -1, -1, -1, -1],
     ],
     answer: 3,
   };
-  const [boardState, setBoardState] = useState(problem);
+  const [boardState, setBoardState] = useState(initialProblem);
+
+  const getRandomNumber = (): [number, number] => {
+    const numbers = [1, 2, 3, 4, 5];
+    const sections = [1, 2, 3, 4];
+    const numIndex = Math.floor(Math.random() * numbers.length);
+    const sectionIndex = Math.floor(Math.random() * sections.length);
+    const selectedNumber = numbers.splice(numIndex, 1)[0];
+    const selectedSection = sections.splice(sectionIndex, 1)[0];
+    if (selectedSection === 1) {
+      return [selectedNumber, 0];
+    } else if (selectedSection === 2) {
+      return [selectedNumber, 6];
+    } else if (selectedSection === 3) {
+      return [6, selectedNumber];
+    } else if (selectedSection === 4) {
+      return [0, selectedNumber];
+    }
+  };
+
+  const generateProblem = (): void => {
+    const problem: Array<Array<number>> = Array(7)
+      .fill(0)
+      .map((_, i) =>
+        Array(7)
+          .fill(0)
+          .map((_, j) => {
+            if (i === 0 || i === 6 || j === 0 || j === 6) {
+              return -1;
+            }
+            return 0;
+          })
+      );
+
+    const destinations = [1, 2, 3, 1, 2, 3];
+
+    while (destinations.length > 0) {
+      const [x, y] = getRandomNumber();
+      if (problem[y][x] === -1) {
+        problem[y][x] = destinations.pop()!;
+      }
+    }
+
+    const newProblem: Problem = {
+      id: boardState.id + 1,
+      items: problem,
+      answer: 25,
+    };
+    setBoardState(newProblem);
+  };
 
   const onClickHandler = (
     event: MouseEvent,
@@ -47,47 +97,52 @@ const GameBoard: React.FC = () => {
   };
 
   return (
-    <React.Fragment>
-      {boardState.items.map((item, yIndex) => {
-        return (
-          <RowFlexBox key={yIndex}>
-            {item.map((rowValue, xIndex) => {
-              if (rowValue === 0 || rowValue === 4 || rowValue === 5) {
-                return (
-                  <EmptyBox attribute={rowValue} key={xIndex}>
-                    <LeftTopDiagonal
+    <RowFlexBox>
+      <div>
+        {boardState.items.map((item, yIndex) => {
+          return (
+            <RowFlexBox key={yIndex}>
+              {item.map((rowValue, xIndex) => {
+                if (rowValue === 0 || rowValue === 4 || rowValue === 5) {
+                  return (
+                    <EmptyBox attribute={rowValue} key={xIndex}>
+                      <LeftTopDiagonal
+                        attribute={rowValue}
+                        xIndex={xIndex}
+                        yIndex={yIndex}
+                        onClick={(event: MouseEvent) =>
+                          onClickHandler(event, xIndex, yIndex, 4)
+                        }
+                      />
+                      <RightTopDiagonal
+                        attribute={rowValue}
+                        xIndex={xIndex}
+                        yIndex={yIndex}
+                        onClick={(event: MouseEvent) =>
+                          onClickHandler(event, xIndex, yIndex, 5)
+                        }
+                      />
+                    </EmptyBox>
+                  );
+                } else {
+                  return (
+                    <EmptyBox
                       attribute={rowValue}
                       xIndex={xIndex}
                       yIndex={yIndex}
-                      onClick={(event: MouseEvent) =>
-                        onClickHandler(event, xIndex, yIndex, 4)
-                      }
+                      key={xIndex}
                     />
-                    <RightTopDiagonal
-                      attribute={rowValue}
-                      xIndex={xIndex}
-                      yIndex={yIndex}
-                      onClick={(event: MouseEvent) =>
-                        onClickHandler(event, xIndex, yIndex, 5)
-                      }
-                    />
-                  </EmptyBox>
-                );
-              } else {
-                return (
-                  <EmptyBox
-                    attribute={rowValue}
-                    xIndex={xIndex}
-                    yIndex={yIndex}
-                    key={xIndex}
-                  />
-                );
-              }
-            })}
-          </RowFlexBox>
-        );
-      })}
-    </React.Fragment>
+                  );
+                }
+              })}
+            </RowFlexBox>
+          );
+        })}
+      </div>
+      <button style={{ height: "3rem" }} onClick={generateProblem}>
+        새 문제
+      </button>
+    </RowFlexBox>
   );
 };
 
--- src/components/GamePage.tsx
+++ src/components/GamePage.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import GameBoard from "./GameBoard";
+import styled from "styled-components";
 
 const GamePage: React.FC = () => {
-  return <GameBoard />;
+  return (
+    <GameBox>
+      <GameBoard />
+    </GameBox>
+  );
 };
+
+const GameBox = styled.div`
+  margin: 2rem;
+`;
 
 export default GamePage;
